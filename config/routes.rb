9b30350b68d6dc03ca99eff470f31e6b1a079f6e
Krishna <libraryismyparadise@gmail.com>
2023-03-21 17:17:56 +0545

    Add Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 devise_for :users, controllers: { registrations: 'users/registrations' }, sign_out_via: [:get, :delete]
-   root "landing_page#index"
+   root "groups#index"
     resources :groups do
     resources :expenses
   end
