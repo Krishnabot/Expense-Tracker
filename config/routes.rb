5d0013cb39ee8fcae1d6d629bc86425d282f204d
Krishna <libraryismyparadise@gmail.com>
2023-03-22 21:28:04 +0545

    Style welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 devise_for :users, controllers: { registrations: 'users/registrations' }, sign_out_via: [:get, :delete]
-   root "groups#index"
+   root "welcome#index"
     resources :groups do
     resources :expenses
   end
