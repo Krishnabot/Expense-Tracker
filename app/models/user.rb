4ac16c161fb00036ddcb4c0e717f16f49abefb1b
Krishna <libraryismyparadise@gmail.com>
2023-03-22 18:23:56 +0545

    Fix Font Family

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :operations, foreign_key: 'user_id', dependent: :destroy
+  has_many :expenses, foreign_key: 'user_id', dependent: :destroy
   has_many :groups, foreign_key: 'user_id', dependent: :destroy
   validates :name, presence: true, length: { maximum: 250 }
   validates :password, presence: true, length: { minimum: 6 }
