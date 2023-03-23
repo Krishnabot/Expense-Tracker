6363798289c1b0bb7b3684ad50b22fe4b2aac651
Krishna <libraryismyparadise@gmail.com>
2023-03-23 19:34:43 +0545

    Fix Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-devise_for :users, controllers: { registrations: 'users/registrations' }, sign_out_via: [:get, :delete]
-   root "welcome#index"
-    resources :groups do
-    resources :expenses
+  devise_for :users, controllers: { registrations: 'users/registrations' }, sign_out_via: [:get, :delete]
+  root "welcome#index"
+  resources :groups, only: [:index, :show, :new, :create] do
+    resources :expenses, only: [:new, :create]
   end
 end
