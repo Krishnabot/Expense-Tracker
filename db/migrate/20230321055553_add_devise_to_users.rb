2a1b0808231f4ae6b8ae65776277fd135982f11e
Krishna <libraryismyparadise@gmail.com>
2026-01-21 11:40:01 +0900

    Standardize string quotes and array syntax

--- db/migrate/20230321055553_add_devise_to_users.rb
+++ db/migrate/20230321055553_add_devise_to_users.rb
@@ -14,14 +14,14 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+       ## Trackable
+       # t.integer  :sign_in_count, default: 0, null: false
+       # t.datetime :current_sign_in_at
+       # t.datetime :last_sign_in_at
+       # t.string   :current_sign_in_ip
+       # t.string   :last_sign_in_ip
 
-      ## Confirmable
+       ## Confirmable
        t.string   :confirmation_token
        t.datetime :confirmed_at
        t.datetime :confirmation_sent_at
