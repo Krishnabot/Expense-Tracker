f2506cfc2807f36a9dfd81cd18df4fee29c5e01e
Krishna <libraryismyparadise@gmail.com>
2026-01-21 14:26:28 +0900

    Update Ruby version and dependencies for Rails 8

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_many :group_expenses, foreign_key: 'group_id'
+  belongs_to :user, class_name: "User", foreign_key: "user_id"
+  has_many :group_expenses, foreign_key: "group_id"
   has_and_belongs_to_many :groups
 
   validates :name, presence: true, length: { maximum: 250 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :expenses, foreign_key: 'user_id', dependent: :destroy
-  has_many :groups, foreign_key: 'user_id', dependent: :destroy
+  has_many :expenses, foreign_key: "user_id", dependent: :destroy
+  has_many :groups, foreign_key: "user_id", dependent: :destroy
   validates :name, presence: true, length: { maximum: 250 }
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true
